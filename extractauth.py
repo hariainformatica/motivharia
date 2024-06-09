d7cf9c57a7f73dba65403dcc251d972582a3c62c
Haría Emilio <haria.informatica@gmail.com>
2024-06-09 15:59:24 +0100

    get all authors

--- extractauth.py
+++ extractauth.py
@@ -1,12 +1,22 @@
 import re
 
+autores = []
 regex = r"(?<=\().*(?=\))"
 
-#get the text from the file frases.txt and store it in the variable test_str
 with open('frases.txt', 'r') as file:
     test_str = file.read()
 
 matches = re.finditer(regex, test_str, re.MULTILINE)
-
 for matchNum, match in enumerate(matches, start=1):
     print ("Match {matchNum}: {match}".format(matchNum = matchNum, match = match.group()))
+    #if author is not in the list, add it
+    if match.group() not in autores:
+        autores.append(match.group())
+
+print("\n\n\nAutores:\n----------")
+
+cont = 1
+autores.sort()
+for autor in autores:
+    print(f'{cont}:{autor}')
+    cont += 1
