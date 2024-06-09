10905a93b03fecadad39113e8e394a1f4b9b6db1
Haría Emilio <haria.informatica@gmail.com>
2024-06-09 15:46:58 +0100

    remove comments

--- extractauth.py
+++ extractauth.py
@@ -1,6 +1,3 @@
-# coding=utf8
-# the above tag defines encoding for this document and is for Python 2.x compatibility
-
 import re
 
 regex = r"(?<=\().*(?=\))"
@@ -19,5 +16,3 @@ for matchNum, match in enumerate(matches, start=1):
         groupNum = groupNum + 1
         
         print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
-
-# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
