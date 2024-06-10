1197fcbe6bcc435f9cd730f5cec846538f9b380c
Haría Emilio <haria.informatica@gmail.com>
2024-06-10 07:23:35 +0100

    no corre textual si se llama textual

--- textuals.py
+++ textuals.py
@@ -2,8 +2,8 @@ from textual.app import App
 from textual.widgets import Placeholder
 
 class MiAplicacion(App):
-    def compose(self) -> None:
-        yield Placeholder("Hola Mundo", justify="center")
+    def compose(self):
+        yield Placeholder("Hola Mundo")
 
 app = MiAplicacion()
 app.run()
